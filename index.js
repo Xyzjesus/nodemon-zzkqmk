f721b418837365c94e6d644bbd9962417424a141
Xyzjesus <auroracoffeeroaster@gmail.com>
2022-08-26 18:31:37 +0400

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
 
-const token = '5530408809:AAHnTHfO3w6FcH_8HoVhH8JJPAOpP25vPgs'; // тут токен кторый мы получили от botFather
+const token = ''; // тут токен кторый мы получили от botFather
 
 // включаем самого обота
 const bot = new TelegramBot(token, { polling: true });
