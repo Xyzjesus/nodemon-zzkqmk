08458b2cc88f0ee879b0a4a1beab59c5f0eb90d2
Xyzjesus <konstprakhov@gmail.com>
2022-10-31 19:13:25 +0000

    update

--- composers/fc_editors.composer.js
+++ composers/fc_editors.composer.js
@@ -8,9 +8,9 @@ composer.command('course', async (ctx) => {
     await ctx.replyWithHTML(
       ctx.i18n.t('summary'), //TODO counter with storage of learners
       Markup.inlineKeyboard([
-        [Markup.button.callback('Easy', 'category1_btn4')],
-        [Markup.button.callback('Medium', 'category1_btn5')],
-        [Markup.button.callback('Hard', 'category1_btn11')],
+        [Markup.button.callback('Easy', 'category1_btn11')],
+        [Markup.button.callback('Medium', 'category1_btn4')],
+        [Markup.button.callback('Hard', 'category1_btn5')],
       ])
     );
   } catch (e) {
@@ -39,6 +39,20 @@ composer.action('category1_btn5', async (ctx) => {
       ctx.i18n.t('tarif2'), //TODO counter with storage of learners
       Markup.inlineKeyboard([
         [Markup.button.callback('Способы оплаты', 'category1_btn7')],
+      ])
+    );
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+composer.action('category1_btn11', async (ctx) => {
+  try {
+    await ctx.answerCbQuery();
+    await ctx.replyWithHTML(
+      ctx.i18n.t('tarif3'),
+      Markup.inlineKeyboard([
+        [Markup.button.callback('Способы оплаты', 'category1_btn12')],
       ])
     );
   } catch (e) {
@@ -81,6 +95,27 @@ composer.action('category1_btn7', async (ctx) => {
           ),
         ],
         [Markup.button.callback('Криптовалюта 230USDT', 'category1_btn10')],
+      ])
+    );
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+composer.action('category1_btn12', async (ctx) => {
+  try {
+    await ctx.answerCbQuery();
+    await ctx.replyWithHTML(
+      ctx.i18n.t('sposobioplati'),
+      Markup.inlineKeyboard([
+        [Markup.button.callback('Иностранная карта 165$', 'category1_btn8')],
+        [
+          Markup.button.callback(
+            'Российская карта 10000 рублей',
+            'category1_btn9'
+          ),
+        ],
+        [Markup.button.callback('Криптовалюта 165USDT', 'category1_btn10')],
       ])
     );
   } catch (e) {
